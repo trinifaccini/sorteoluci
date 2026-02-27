c07003bcf74fcd05afdf97b32c32fc234cc44a0d
Trini Faccini <78211732+trinifaccini@users.noreply.github.com>
2026-02-26 23:58:03 -0300

    Update Welcome.jsx

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,7 +1,13 @@
 import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+
 
 export default function Welcome() {
   const navigate = useNavigate()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
 
   return (
     <div className="welcome">
